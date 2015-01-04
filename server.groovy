dbd1fb58ddabc308c3e0f45a96e8ecc7596ba7e7
Sridhar Sarnobat <ss401533@gmail.com>
2015-01-03 18:07:19 -0800

    Minor

--- server.groovy
+++ server.groovy
@@ -10,8 +10,9 @@ import com.sun.syndication.io.SyndFeedInput;
 import com.sun.syndication.io.SyndFeedOutput;
 import com.sun.syndication.io.XmlReader;
 
-import javax.ws.rs.core.Response
+import javax.ws.rs.core.*;
 import java.io.IOException;
+import java.net.MalformedURLException;
 import java.net.URI;
 import java.net.URISyntaxException;
 
@@ -24,10 +25,8 @@ import org.glassfish.jersey.server.ResourceConfig;
 
 import com.sun.net.httpserver.HttpServer;
 
-// TODO: Add Pat McNeill and Flashback and livecast, Parks
 @Path("helloworld")
-public class HelloWorldResource {
-
+public class Server {
 
 	@Path("hello")
 	@GET
@@ -40,101 +39,133 @@ public class HelloWorldResource {
 	@GET
 	@Produces("text/plain")
 	public Response wkh() {
-		return  Response.ok(getFeed("http://www.pwtorch.com/pwtorchvipaudio.xml", "otline", "lashback")).build();
+		return Response.ok(
+				getFeed("http://www.pwtorch.com/pwtorchvipaudio.xml", "otline",
+						"lashback")).build();
 	}
 
 	@Path("int")
 	@GET
 	@Produces("text/plain")
 	public Response interview() {
-		return  Response.ok(getFeed("http://www.pwtorch.com/pwtorchvipaudio.xml", "Livecast Interview", "foobar")).build();
+		return Response.ok(
+				getFeed("http://www.pwtorch.com/pwtorchvipaudio.xml",
+						"Livecast (. VIP Aftershow)? w/", "foobar")).build();
 	}
 
 	@Path("jc")
 	@GET
 	@Produces("text/plain")
 	public Response jamesCaldwell() {
-		return  Response.ok(getFeed("http://www.pwtorch.com/pwtorchvipaudio.xml", "Caldwell", "foobar")).build();
+		return Response.ok(
+				getFeed("http://www.pwtorch.com/pwtorchvipaudio.xml",
+						"Caldwell", "foobar")).build();
 	}
 
 	@Path("bm")
 	@GET
 	@Produces("text/plain")
 	public Response bm() {
-		return  Response.ok(getFeed("http://www.pwtorch.com/pwtorchvipaudio.xml", "Bruce Mitchell Audio", "foobar")).build();
+		return Response.ok(
+				getFeed("http://www.pwtorch.com/pwtorchvipaudio.xml",
+						"Bruce Mitchell Audio", "foobar")).build();
 	}
 
-	
-        @Path("greg")
-        @GET
-        @Produces("text/plain")
-        public Response greg() {
-                return  Response.ok(getFeed("http://www.pwtorch.com/pwtorchvipaudio.xml", "arks", "aldwell")).build();
-        }
+	@Path("greg")
+	@GET
+	@Produces("text/plain")
+	public Response greg() {
+		return Response.ok(
+				getFeed("http://www.pwtorch.com/pwtorchvipaudio.xml", "arks",
+						"aldwell")).build();
+	}
 
-        @Path("flashback")
-        @GET
-        @Produces("text/plain")
-        public Response flashback() {
-                return  Response.ok(getFeed("http://www.pwtorch.com/pwtorchvipaudio.xml", "lashback", "foobar")).build();
-        }
+	@Path("flashback")
+	@GET
+	@Produces("text/plain")
+	public Response flashback() {
+		return Response.ok(
+				getFeed("http://www.pwtorch.com/pwtorchvipaudio.xml",
+						"lashback", "foobar")).build();
+	}
 
-        @Path("livecast")
-        @GET
-        @Produces("text/plain")
-        public Response livecast() {
-                return  Response.ok(getFeed("http://www.pwtorch.com/pwtorchvipaudio.xml", "ivecast", "foobar")).build();
-        }
+	@Path("livecast")
+	@GET
+	@Produces("text/plain")
+	public Response livecast() {
+		return Response.ok(
+				getFeed("http://www.pwtorch.com/pwtorchvipaudio.xml",
+						"ivecast", "foobar")).build();
+	}
 
-        @Path("pat")
-        @GET
-        @Produces("text/plain")
-        public Response patMcneill() {
-                return  Response.ok(getFeed("http://www.pwtorch.com/pwtorchvipaudio.xml", "eill", "foobar")).build();
-        }
+	@Path("pat")
+	@GET
+	@Produces("text/plain")
+	public Response patMcneill() {
+		return Response.ok(
+				getFeed("http://www.pwtorch.com/pwtorchvipaudio.xml", "eill",
+						"foobar")).build();
+	}
 
-        @Path("powell")
-        @GET
-        @Produces("text/plain")
-        public Response jamesPowell() {
-                return  Response.ok(getFeed("http://www.pwtorch.com/pwtorchvipaudio.xml", "owell", "foobar")).build();
-        }
+	@Path("powell")
+	@GET
+	@Produces("text/plain")
+	public Response jamesPowell() {
+		return Response.ok(
+				getFeed("http://www.pwtorch.com/pwtorchvipaudio.xml", "owell",
+						"foobar")).build();
+	}
 
 	@Path("test")
 	@GET
 	@Produces("text/plain")
 	public Response test() {
-		return  Response.ok(getFeed("http://www.pwtorch.com/pwtorchvipaudio.xml", "otline", "lashback")).build();
+		return Response.ok(
+				getFeed("http://www.pwtorch.com/pwtorchvipaudio.xml", "otline",
+						"lashback")).build();
 	}
-	
-	private String getFeed(String url, String include, String exclude) throws CloneNotSupportedException, IOException,
-		FeedException {
-		SyndFeed feed = new SyndFeedInput().build(new XmlReader(new URL(
-				url)));
-		SyndFeed wkHotlineFeed = (SyndFeed) feed.clone();
-		for (Object o : feed.getEntries()) {
-			SyndEntry entry = (SyndEntry) o;
-			if (entry.getTitle().contains(include)) {
-				if (!entry.getTitle().contains(exclude)) {
-					continue;
+
+	private String getFeed(String url, String include, String exclude) {
+		SyndFeed feed;
+		try {
+			feed = new SyndFeedInput().build(new XmlReader(new URL(url)));
+
+			SyndFeed wkHotlineFeed = (SyndFeed) feed.clone();
+			for (Object o : feed.getEntries()) {
+				SyndEntry entry = (SyndEntry) o;
+				if (entry.getTitle().matches(".*" + include + ".*")) {
+					if (!entry.getTitle().contains(exclude)) {
+						continue;
+					}
 				}
+				wkHotlineFeed.getEntries().remove(o);
 			}
-			wkHotlineFeed.getEntries().remove(o);
+			ByteArrayOutputStream bao = new ByteArrayOutputStream();
+			OutputStreamWriter outputStreamWriter = new OutputStreamWriter(bao);
+			new SyndFeedOutput().output(wkHotlineFeed, outputStreamWriter);
+			outputStreamWriter.close();
+			return new String(bao.toByteArray());
+		} catch (Exception e) {
+			throw new RuntimeException(e);
 		}
-		ByteArrayOutputStream bao = new ByteArrayOutputStream();
-		OutputStreamWriter outputStreamWriter = new OutputStreamWriter(bao);
-		new SyndFeedOutput().output(wkHotlineFeed, outputStreamWriter);
-		outputStreamWriter.close();
-		return new String(bao.toByteArray());
 	}
 
+	public static void main(String[] args) throws URISyntaxException {
+
+		if ("VIP - 02/14 PWTorch Livecast w/Jonny Fairplay: Never-told stories on what went wrong in TNA ten years ago, Punk stories, Heenans big compliment"
+				.matches(".*Livecast( . VIP Aftershow)? w/.*")) {
+			System.out.println("regex is correct");
+		} else {
+			System.out.println("regex is not correct");
+		}
+
+		String BASE_URI = "http://localhost:4444/";
+
+		ResourceConfig rc = new ResourceConfig(Server.class);
+		URI endpoint = new URI(BASE_URI);
+
+		HttpServer server = JdkHttpServerFactory.createHttpServer(endpoint, rc);
+		System.out.println("console v2.0 : Press Enter to stop the server. ");
+
+	}
 }
-
-
-String BASE_URI = "http://localhost:4444/";
-
-ResourceConfig rc = new ResourceConfig(HelloWorldResource.class);
-URI endpoint = new URI(BASE_URI);
-
-HttpServer server = JdkHttpServerFactory.createHttpServer(endpoint, rc);
-System.out.println("console v2.0 : Press Enter to stop the server. ");
